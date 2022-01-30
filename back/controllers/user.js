774cdafae4bd236952e7e6a39d2b4a8b1494bf2f
Myriam Daho <myriam.daho@yahoo.fr>
2022-01-30 13:20:47 +0100

    deposer livrable

---
+++ back/controllers/user.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt');
+const cryptojs = require('crypto-js');
+const jwt = require ('jsonwebtoken');
+const User = require('../models/user');
+
+require('dotenv').config();
+
+exports.signup = (req, res, next) =>{
+const emailCryptoJs= cryptojs.HmacSHA256(req.body.email,process.env.ENCRYPT_EMAIL).toString();
+
+bcrypt.hash(req.body.password, 10)
+.then(hash => {
+    const user = new User({
+        email: emailCryptoJs,
+        password: hash
+    });
+    user.save()
+     .then(() =>res.status(201).json({ message:'Utilisateur créé !'}))
+     .catch(error => res.status(400).json({error}));
+  })
+.catch(error => res.status(500).json({error}));
+};
+
+exports.login = (req, res, next) => {
+  const emailCryptoJs= cryptojs.HmacSHA256(req.body.email,process.env.ENCRYPT_EMAIL).toString();
+    User.findOne({ email: emailCryptoJs })
+     .then(user =>{
+       if(!user){
+        return res.status(401).json({ error:'Utilisateur non trouvé !' });
+       }
+             
+       bcrypt.compare(req.body.password, user.password)
+         .then(valid => {
+           if (!valid) {
+             return res.status(401).json ({error: 'mot de passe incorrect !'});
+           }
+        res.status(200).json({
+          userId: user._id,
+          token:jwt.sign(
+            { userId: user._id},
+            process.env.TOKEN_SECRET,
+            { expiresIn:'24h'} 
+          )
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
